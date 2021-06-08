aa3a232f464f1f8cecb43d4fbbb2022b4d40df5f
Mas Joe <56056808+klikdesainwebcom@users.noreply.github.com>
2021-06-09 04:36:41 +0700

    Update product.js

--- product.js
+++ product.js
@@ -142,7 +142,7 @@ document.getElementById('txthp').value = '';
 document.addEventListener('DOMContentLoaded', function() {
 var xVideo1 = document.getElementById("video1").getAttribute("src");
 var xVideo2 = document.getElementById("video1").getAttribute("alt");
-var xImg = document.getElementById("video1").getAttribute("src");
+var xImg = document.getElementById("pic1").getAttribute("src");
 if (xVideo2){
   var xPreview = '<amp-video id="preview_video" class="detail_prod" width="512" height="512" src="'+ xVideo2 +'" poster="'+ xVideo1 +'" layout="responsive" controls="" autoplay=""></amp-video>';
   document.getElementById("preview").innerHTML = xPreview;
@@ -151,13 +151,25 @@ if (xVideo2){
   document.getElementById("video1").style.display = 'block';
   document.getElementById("video1").style.visibility = 'visible';  
 }
+else if (xImg){
+   var imgPrev = '<amp-img id="preview_img" class="detail_prod" src="'+ xImg +'"  width="512" height="512" alt="" title="" layout="responsive"></amp-img>';
+  document.getElementById("preview").innerHTML = imgPrev;
+  document.getElementById("video1").style.display = 'none';
+  document.getElementById("video1").style.visibility = 'hidden';
+  document.getElementById("pic1").style.display = 'block';
+  document.getElementById("pic1").style.visibility = 'visible';  
+}
 });
 document.getElementById("video1").addEventListener("click", function() { 
 var x1Video = document.getElementById("video1").getAttribute("alt");
 document.getElementById("preview").innerHTML = '<amp-video id="preview_video" width="512" height="512" src="'+ x1Video +'" poster="'+ x1Video +'" layout="responsive" controls="true" autoplay="true"></amp-video>' ;
 document.getElementById("preview_video").src = x1Video;
 });
-
+document.getElementById("pic1").addEventListener("click", function() { 
+var xImg1 = document.getElementById("pic1").getAttribute("src");
+document.getElementById("preview").innerHTML = '<amp-img id="preview_img" src="'+ xImg1 +'" width="512" height="512" alt="" title="" layout="responsive"></amp-img>';
+document.getElementById("preview_img").src = xImg1;
+});
 document.getElementById("pic2").addEventListener("click", function() { 
 var xImg2 = document.getElementById("pic2").getAttribute("src");
 document.getElementById("preview").innerHTML = '<amp-img id="preview_img" src="'+ xImg2 +'" width="512" height="512" alt="" title="" layout="responsive"></amp-img>';
