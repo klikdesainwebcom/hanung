9f3c70559955b4f0736710b6c29ba41dde03c7dc
Mas Joe <56056808+klikdesainwebcom@users.noreply.github.com>
2021-06-09 00:06:39 +0700

    Update product.js

--- product.js
+++ product.js
@@ -149,7 +149,7 @@ document.getElementById("pic1").style.display = 'none';
 document.getElementById("pic1").style.visibility = 'hidden';
 }
 else if (xImg){
-document.getElementById("preview").innerHTML = '<amp-img id="preview_img" layout="responsive" src="'+ xImg +'" class="detail_prod" width="512" height="512" alt="" title="" style="border:1px solid red;"></amp-img>';
+document.getElementById("preview").innerHTML = '<amp-img id="preview_img" layout="responsive" src="'+ xImg +'" class="detail_prod" width="512" height="512" alt="" title=""></amp-img>';
 document.getElementById("video1").style.display = 'none';
 document.getElementById("video1").style.visibility = 'hidden';
 }
@@ -161,31 +161,31 @@ document.getElementById("preview_video").src = x1Video;
 });
 document.getElementById("pic1").addEventListener("click", function() { 
 var xImg1 = document.getElementById("pic1").getAttribute("src");
-document.getElementById("preview").innerHTML = '<amp-img id="preview_img" src="'+ xImg1 +'" width="512" height="512" alt="" title="" style="border:1px solid red;"></amp-img>';
+document.getElementById("preview").innerHTML = '<amp-img id="preview_img" src="'+ xImg1 +'" width="512" height="512" alt="" title="" layout="responsive"></amp-img>';
 document.getElementById("preview_img").src = xImg1;
 });
 document.getElementById("pic2").addEventListener("click", function() { 
 var xImg2 = document.getElementById("pic2").getAttribute("src");
-document.getElementById("preview").innerHTML = '<amp-img id="preview_img" src="'+ xImg2 +'" width="512" height="512" alt="" title="" style="border:1px solid red;"></amp-img>' ;
+document.getElementById("preview").innerHTML = '<amp-img id="preview_img" src="'+ xImg2 +'" width="512" height="512" alt="" title="" layout="responsive"></amp-img>' ;
 document.getElementById("preview_img").src = xImg2;
 });
 document.getElementById("pic3").addEventListener("click", function() { 
 var xImg3 = document.getElementById("pic3").getAttribute("src");
-document.getElementById("preview").innerHTML = '<amp-img id="preview_img" src="'+ xImg3 +'" width="512" height="512" alt="" title="" style="border:1px solid red;"></amp-img>' ;
+document.getElementById("preview").innerHTML = '<amp-img id="preview_img" src="'+ xImg3 +'" width="512" height="512" alt="" title="" layout="responsive"></amp-img>' ;
 document.getElementById("preview_img").src = xImg3;
 });
 document.getElementById("pic4").addEventListener("click", function() { 
 var xImg4 = document.getElementById("pic4").getAttribute("src");
 document.getElementById("preview_img").src = xImg4;
-document.getElementById("preview").innerHTML = '<amp-img id="preview_img" src="'+ xImg4 +'" width="512" height="512" alt="" title="" style="border:1px solid red;"></amp-img>' ;
+document.getElementById("preview").innerHTML = '<amp-img id="preview_img" src="'+ xImg4 +'" width="512" height="512" alt="" title="" layout="responsive"></amp-img>' ;
 });
 document.getElementById("pic5").addEventListener("click", function() { 
 var xImg5 = document.getElementById("pic5").getAttribute("src");
-document.getElementById("preview").innerHTML = '<amp-img id="preview_img" src="'+ xImg5 +'" width="512" height="512" alt="" title="" style="border:1px solid red;"></amp-img>' ;
+document.getElementById("preview").innerHTML = '<amp-img id="preview_img" src="'+ xImg5 +'" width="512" height="512" alt="" title="" layout="responsive"></amp-img>' ;
 document.getElementById("preview_img").src = xImg5;
 });
 document.getElementById("pic6").addEventListener("click", function() { 
 var xImg5 = document.getElementById("pic6").getAttribute("src");
-document.getElementById("preview").innerHTML = '<amp-img id="preview_img" src="'+ xImg5 +'" width="512" height="512" alt="" title="" style="border:1px solid red;"></amp-img>' ;
+document.getElementById("preview").innerHTML = '<amp-img id="preview_img" src="'+ xImg5 +'" width="512" height="512" alt="" title="" layout="responsive"></amp-img>' ;
 document.getElementById("preview_img").src = xImg5;
 });
