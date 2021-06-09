431325d0a9d4f417401440cb2ad4855e71febdca
Mas Joe <56056808+klikdesainwebcom@users.noreply.github.com>
2021-06-09 14:16:08 +0700

    Update joe.js

--- joe.js
+++ joe.js
@@ -3,12 +3,12 @@ document.addEventListener('DOMContentLoaded', function() {
 	var xVideo2 = document.getElementById("video1").getAttribute("alt");
 	var xImg = document.getElementById("pic1").getAttribute("src");
 	if (xVideo2){
-		document.getElementById("preview").innerHTML = '<video id="preview_video" class="detail_prod" width="512" height="512" src="'+ xVideo1 +'" poster="'+ xVideo2 +'" layout="responsive" controls="" autoplay=""></video>' ;
+		document.getElementById("preview").innerHTML = '<video id="preview_video" class="detail_prod" width="512" height="512" src="'+ xVideo1 +'" poster="'+ xVideo2 +'" layout="responsive" controls autoplay></video>' ;
 		document.getElementById("pic1").style.display = 'none';
 		document.getElementById("pic1").style.visibility = 'hidden';
 		}
 	else if (xImg){
-		document.getElementById("preview").innerHTML = '<img id="preview_img" layout="responsive" src="'+ xImg +'" class="detail_prod" width="512" height="512" alt="" title="" style="border:1px solid red;"/>' ;
+		document.getElementById("preview").innerHTML = '<img id="preview_img" layout="responsive" src="'+ xImg +'" class="detail_prod" width="512" height="512" alt="" title=""/>' ;
 		document.getElementById("video1").style.display = 'none';
 		document.getElementById("video1").style.visibility = 'hidden';
 		}
